fade34503520fd943fb1d72ac4550548ed579223
nguyn21012003 <66507352+nguyn21012003@users.noreply.github.com>
2025-11-10 18:58:14 +0700

    this hamiltonian function now have its gradient

--- core/genHam.py
+++ core/genHam.py
@@ -5,7 +5,8 @@ from core.irrMatrixTransform import IR as IR_tran
 from core.irrMatrixTransform import IRNN as IRNN_tran
 from core.irrMatrixTransform import IRTNN as IRTNN_tran
 
-def tbm_Hamiltonian(alpha, beta, data, modelNeighbor):
+
+def tbm_Hamiltonian(alpha, beta, data, modelNeighbor, a):
     E0, h1, h2, h3, h4, h5, h6 = IR_tran(data)
     ham = (
         E0
@@ -16,6 +17,21 @@ def tbm_Hamiltonian(alpha, beta, data, modelNeighbor):
         + exp(1j * (-alpha + beta)) * h5
         + exp(1j * (alpha + beta)) * h6
     )
+    dhkx = (
+        1j * a * exp(1j * 2 * alpha) * h1
+        + 1j * a / 2 * exp(1j * (alpha - beta)) * h2
+        - 1j * a / 2 * exp(-1j * (alpha + beta)) * h3
+        - 1j * a * exp(-1j * 2 * alpha) * h4
+        - 1j * a / 2 * exp(1j * (-alpha + beta)) * h5
+        + 1j * a / 2 * exp(1j * (alpha + beta)) * h6
+    )
+    dhky = (
+        1j * a * sqrt(3) / 2 * exp(1j * (alpha - beta)) * h2
+        + 1j * a * sqrt(3) / 2 * exp(-1j * (alpha + beta)) * h3
+        - 1j * a * sqrt(3) / 2 * exp(1j * (-alpha + beta)) * h5
+        - 1j * a * sqrt(3) / 2 * exp(1j * (alpha + beta)) * h6
+    )
+
     if modelNeighbor == "TNN":
         o1, o2, o3, o4, o5, o6 = IRTNN_tran(data)
         v1, v2, v3, v4, v5, v6 = IRNN_tran(data)
@@ -34,4 +50,40 @@ def tbm_Hamiltonian(alpha, beta, data, modelNeighbor):
             + exp(1j * (2 * beta)) * v5
             + exp(1j * (3 * alpha + beta)) * v6
         )
-    return ham
+        dhkx += (
+            1j
+            * a
+            / 2
+            * (
+                4 * exp(4j * alpha) * o1
+                + 2 * exp(2j * (alpha - beta)) * o2
+                - 2 * exp(2j * (-alpha - beta)) * o3
+                - 4 * exp(-4j * alpha) * o4
+                - 2 * exp(2j * (-alpha + beta)) * o5
+                + 2 * exp(2j * (alpha + beta)) * o6
+                + 3 * exp(1j * (3 * alpha - beta)) * v1
+                - 3 * exp(1j * (-3 * alpha - beta)) * v3
+                - 3 * exp(1j * (-3 * alpha + beta)) * v4
+                + 3 * exp(1j * (3 * alpha + beta)) * v6
+            )
+        )
+        dhky += (
+            1j
+            * sqrt(3)
+            * a
+            / 2
+            * (
+                -2 * exp(2j * (alpha - beta)) * o2
+                - 2 * exp(2j * (-alpha - beta)) * o3
+                + 2 * exp(2j * (-alpha + beta)) * o5
+                + 2 * exp(2j * (alpha + beta)) * o6
+                - exp(1j * (3 * alpha - beta)) * v1
+                - 2 * exp(1j * (-2 * beta)) * v2
+                - exp(1j * (-3 * alpha - beta)) * v3
+                + exp(1j * (-3 * alpha + beta)) * v4
+                + 2 * exp(1j * (2 * beta)) * v5
+                + exp(1j * (3 * alpha + beta)) * v6
+            )
+        )
+
+    return ham,dhkx,dhky
